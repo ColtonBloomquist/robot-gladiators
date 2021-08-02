291717f555dc5e2bdcdae3e6f46124d45e718ac9
colton bloomquist <coltonbloomquist@yahoo.com>
2021-08-01 18:46:07 -0600

    added enemies and for loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,16 +4,14 @@ var playerAttack = 10;
 var playerMoney = 10;
 
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
-    window.alert("Welcome to Robot Gladiators!");
-};
+var fight = function(enemyNames) {
+    
 
-fight();
-
+window.alert("Welcome to Robot Gladiators!");
 var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
  // if player choses to fight, then fight
@@ -21,20 +19,20 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
     console.log(
-      playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+      playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
     );
   
     // check enemy's health
     if (enemyHealth <= 0) {
-      window.alert(enemyName + " has died!");
+      window.alert(enemyNames + " has died!");
     } else {
-      window.alert(enemyName + " still has " + enemyHealth + " health left.");
+      window.alert(enemyNames + " still has " + enemyHealth + " health left.");
     }
   
     // remove player's health by subtracting the amount set in the enemyAttack variable
     playerHealth = playerHealth - enemyAttack;
     console.log(
-      enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+      enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
   
     // check player's health
@@ -62,4 +60,11 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     window.alert(playerName + " has chosen to skip the fight!");
  } else {
     window.alert("You need to choose a valid option. Try again!");
+  }
+
+};
+
+
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
   }
